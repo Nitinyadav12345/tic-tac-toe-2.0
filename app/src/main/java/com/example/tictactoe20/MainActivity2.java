edfa51e72c16b7ddfaae1bc33db56882d4362051
Nitinyadav12345 <70438455+Nitinyadav12345@users.noreply.github.com>
2022-01-19 03:06:36 +0530

    Update MainActivity2.java

--- app/src/main/java/com/example/tictactoe20/MainActivity2.java
+++ app/src/main/java/com/example/tictactoe20/MainActivity2.java
@@ -13,7 +13,7 @@ public class MainActivity2 extends AppCompatActivity {
     int activeplayer = 0;
     // 1->0 0->x 2->null
     int[] gamestate = {2,2,2,2,2,2,2,2,2};
-    int[][] winpositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,8},{2,5,7},{0,4,8},{2,4,6}};
+    int[][] winpositions = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public void playerTap(View view){
         boolean x=false;
